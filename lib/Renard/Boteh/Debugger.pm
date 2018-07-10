66d652a59756c7867487f92dd7dbca19ada5ed71
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-07-10 13:29:32 -0400

    Initial commit

---
+++ lib/Renard/Boteh/Debugger.pm
@@ -0,0 +1,9 @@
+package Renard::Boteh::Debugger;
+# ABSTRACT: A debugger for Boteh motifs
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Boteh-Debugger/>
+
+=cut
